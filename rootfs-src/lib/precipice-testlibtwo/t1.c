b4791f863ef49c2b48336c40acb22fa40fa7515a
macro-6000 <179749124+macro-6000@users.noreply.github.com>
2026-06-02 17:52:51 +0100

    Added a second test library to test the recursive compilation tool

---
+++ rootfs-src/lib/precipice-testlibtwo/t1.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2026, Marko Acres and the Precipice Contributors.
+ * This file is part of the Precipice Operating System project.
+ * Licensed strictly under the terms of the GNU General Public License 
+ * Version 2 ONLY, as specified in the LICENSE file at the root of this 
+ * repository.
+ *
+ * You do NOT have permission to upgrade, modify, or redistribute this 
+ * code under any other version of the GPL (such as v3.x or beyond).
+ *
+ * See precipice/LICENSE for full license.
+ * 
+ */
+
+#include <stdio.h>
+
+void testOne() {
+	printf("Precipice Library Test One");
+
+};
